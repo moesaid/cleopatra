27a5477611e4005f969313e5a086df0d26817171
Mohamed <masedup@gmail.com>
2020-03-05 22:42:07 -0500

    alert scaffold

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+var alert_button = document.getElementsByClassName("alert-btn-close");
+
+for (let i = 0; i < alert_button.length; i++) {
+    const btn = alert_button[i];
+
+    btn.addEventListener('click' , function(){
+        var dad = this.parentNode;
+        dad.classList.add('animated' , 'fadeOut');
+        setTimeout(() => {
+            dad.remove();
+        }, 1000);
+    });
+    
+}
 // check if the page have dropdwon menu
 var dropdown = document.getElementsByClassName('dropdown');
 
