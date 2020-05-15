f2a4610015edc76e7957249cc65435bdfa4925ef
Mohamed <masedup@gmail.com>
2020-05-14 22:11:45 -0400

    new index

--- js/scripts.js
+++ js/scripts.js
@@ -152,6 +152,11 @@ var num = function(from , to){
     return Math.floor(Math.random() * to)  + from;
 };
 
+// array of number
+var numArr = function(length , max){
+    return Array.from({length: length}, () => Math.floor(Math.random() * max));
+}
+
 
 // return 2 digit
 var el_2 = document.getElementsByClassName('num-2');
@@ -252,6 +257,168 @@ var btn     = document.getElementById('sliderBtn'),
     });
 // end with sidebar
 
+var options = function(type, height, numbers , color){
+  return {     
+    chart: {
+      height: height,
+      width: '100%',
+      type: type,
+      sparkline: {
+        enabled: true
+      },
+      toolbar: {
+        show: false,
+       },
+    },
+    grid: {
+        show: false,
+        padding: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0    
+        }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    legend: {
+        show: false,
+    },
+    series: [
+    {
+        name: "serie1",
+        data: numbers
+    }
+    ],    
+    fill: {
+      colors: [color],
+    },
+    stroke:{
+        colors: [color],
+        width: 3
+    },    
+    yaxis: {
+        show: false,        
+    }, 
+    xaxis: {
+      show: false,
+      labels: {
+          show: false,
+      },   
+      axisBorder: {
+        show: false,        
+      },   
+      tooltip: {
+          enabled: false,
+      }
+    },
+    
+  };
+}
+  
+
+  var analytics_1 =  document.getElementsByClassName("analytics_1");
+    
+  if (analytics_1 != null && typeof(analytics_1) != 'undefined') {
+      var chart = new ApexCharts(analytics_1[0], options("area" , '51px' , numArr(10,99) , '#4fd1c5')); 
+      var chart_1 = new ApexCharts(analytics_1[1], options("area" , '51px' , numArr(10,99) , '#4c51bf')); 
+      chart.render();       
+      chart_1.render();       
+  }
+
+
+
+  
+   
+var sealsOptions = {
+    chart: {
+      height: 350,
+      type: "line",
+      stacked: false
+    },
+    dataLabels: {
+      enabled: false
+    },
+    colors: ['#99C2A2', '#C5EDAC', '#66C7F4'],
+    series: [
+      
+      {
+        name: 'Column A',
+        type: 'column',
+        data: [21.1, 23, 33.1, 34, 44.1, 44.9, 56.5, 58.5]
+      },
+      {
+        name: "Column B",
+        type: 'column',
+        data: [10, 19, 27, 26, 34, 35, 40, 38]
+      },
+      {
+        name: "Line C",
+        type: 'column',
+        data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6]
+      },
+    ],
+    stroke: {
+      width: [4, 4, 4]
+    },
+    plotOptions: {
+      bar: {
+        columnWidth: "20%"
+      }
+    },
+    xaxis: {
+      categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+    },
+    yaxis: [
+      {
+        seriesName: 'Column A',
+        axisTicks: {
+          show: true
+        },
+        axisBorder: {
+          show: true,
+        },
+        title: {
+          text: "Columns"
+        }
+      },
+      {
+        seriesName: 'Column A',
+        show: false
+      }, {
+        opposite: true,
+        seriesName: 'Line C',
+        axisTicks: {
+          show: true
+        },
+        axisBorder: {
+          show: true,
+        },
+        title: {
+          text: "Line"
+        }
+      }
+    ],
+    tooltip: {
+      shared: false,
+      intersect: true,
+      x: {
+        show: false
+      }
+    },
+    legend: {
+      horizontalAlign: "left",
+      offsetX: 40
+    }
+  };
+  
+  
+
+
+var sealsOverview = document.getElementById('sealsOverview');
+var sealsOverviewChart = new ApexCharts(sealsOverview, options('bar' , '100%', numArr(20,999) , '#30aba0')); 
+sealsOverviewChart.render();       
 var options = {     
     chart: {
     //   height: 280,
@@ -318,9 +485,14 @@ var options = {
     
   };
   
-  var chart = new ApexCharts(document.querySelector("#SummaryChart"), options);
+
+  var SummaryChart =  document.getElementById("SummaryChart");
   
-  chart.render();
+  if (SummaryChart != null && typeof(SummaryChart) != 'undefined') {
+    var chart = new ApexCharts(document.querySelector("#SummaryChart"), options); 
+    chart.render();
+  }
+  
    
 /* PrismJS 1.19.0
 https://prismjs.com/download.html#themes=prism-tomorrow&languages=markup+css+clike+javascript+http+javadoclike+json+jsonp+json5+markup-templating+php+phpdoc+plsql+python+sass+scss+sql&plugins=unescaped-markup+normalize-whitespace+toolbar+copy-to-clipboard */
