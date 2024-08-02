a3b4bfa05321c0b5fce59e0601ccd60d39ac842f
moesaid <masedup@gmail.com>
2024-08-01 23:15:01 -0400

    Fixed responsive issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./src/views/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: { max: "639px" },
+
+      md: { max: "767px" },
+
+      lg: { max: "1023px" },
+
+      xl: { max: "1279px" },
+    },
     extend: {},
   },
   plugins: [],
